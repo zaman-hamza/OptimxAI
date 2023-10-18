d5839763d6bd9d8d6c7ef293ea6347dc8ffc198f
Naveen <115325063+nkanilku@users.noreply.github.com>
2023-10-18 12:51:13 -0400

    initial commit

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Button from 'components/Button'
-import { actions } from 'slices/app.slice'
 import { images } from 'theme'
 import styles from './dashboard.module.scss'
 
@@ -13,46 +10,8 @@ const Dashboard = () => {
       <div className={styles.container}>
         <img src={images.logo} className={styles.logo} alt="logo" />
         <h3 className={styles.greeting}>{`Hi👋, ${me?.fullName || 'User'}`}</h3>
-        <h1 className={styles.title}>React + Firebase Boilerplate</h1>
-        <p className={styles.description}>
-          This is
-          {'\n'}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/facebook/create-react-app"
-          >
-            create react native app
-          </a>
-          {'\n'}
-          based firebase pre-setup template with basic development setup. For
-          the setup procedure, check the
-          {'\n'}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/WataruMaeda/react-firebase-boilerplate/blob/master/README.md"
-          >
-            README
-          </a>
-          {'\n'}
-          for more information.
-        </p>
-        <div className={styles.buttonContainer}>
-          <Button
-            label="Download for free"
-            className={`btn-purple-fill ${styles.download}`}
-            onClick={() => {
-              window.location.href =
-                'https://github.com/WataruMaeda/react-firebase-boilerplate'
-            }}
-          />
-          <Button
-            label="Logout"
-            className={`btn-purple-outline ${styles.logout}`}
-            onClick={() => dispatch(actions.logout())}
-          />
-        </div>
+        <h1 className={styles.title}>APIKEY = {me?.apiKey}</h1>
+        <h1 className={styles.title}>APIURL = {me?.apiUrl}</h1>
       </div>
     </div>
   )

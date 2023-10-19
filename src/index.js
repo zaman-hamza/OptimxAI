6ba6b1893773338d425c8d9e7c73ef369c5fca9f
Naveen <115325063+nkanilku@users.noreply.github.com>
2023-10-18 20:42:50 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from 'app'
+import App from 'App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'theme/reset.scss'
 import reportWebVitals from './reportWebVitals'
